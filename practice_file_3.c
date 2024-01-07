648add84a33f0b597babf821ec6955e396b81f02
ngwenjeursular <ngwenjeursular@gmail.com>
2024-01-07 12:25:09 -0800

    error handling practice

--- practice_file_3.c
+++ practice_file_3.c
@@ -9,14 +9,29 @@
 double calculateMean(int count, ...)
 {
 	va_list args;
-	double sum = 0;
+	double sum = 0, current;
 	int i;
 
-	va_start(args, count);
+	if (count <= 0)
+	{
+	    fprintf(stderr, "Error: Invalid count for calculateMean function\n");
+	    return -1;
+	}
 
+
+	va_start(args, count);
+	
+	current = va_arg(args, double);
 	for (i = 0; i < count; ++i)
 	{
-		sum += va_arg(args, double);
+		if (!current)
+		{
+		    fprintf(stderr, "Error: Non-double argument for calculateMean function\n");
+		    va_end(args);
+		    return (-1);  /* Return -1 or handle the error as needed */
+		}
+
+		sum += current;
 	}
 
 	va_end(args);
@@ -34,8 +49,15 @@ double calculateMean(int count, ...)
 double calculateVariance(int count, ...)
 {
 	va_list args;
-	double mean, sumSquaredDifferences, difference;
+	double mean, sumSquaredDifferences, difference, current;
 	int i;
+
+	if (count <= 0)
+	{
+	    fprintf(stderr, "Error: Invalid count for calculateVariance function\n");
+	    return (-1);
+	}
+
 
 	va_start(args, count);
 
@@ -49,6 +71,15 @@ double calculateVariance(int count, ...)
 
 	for (i = 0; i < count; ++i)
 	{
+		current = va_arg(args, double);
+
+		if (!current)
+		{
+		    fprintf(stderr, "Error: Non-double argument for calculateVariance function\n");
+		    va_end(args);
+		    return (-1);
+		}
+
 		difference = va_arg(args, double) - mean;
 		sumSquaredDifferences += difference * difference;
 	}
@@ -70,11 +101,25 @@ double calculateStandardDeviation(int count, ...)
 	va_list args;
 	double variance;
 
+	if (count <= 0)
+	{
+	    fprintf(stderr, "Error: Invalid count for calculateStandardDeviation function\n");
+	    return (-1);
+	}
+
+
 	va_start(args, count);
 
 	variance = calculateVariance(count, args);
 
 	va_end(args);
+
+	if (variance == -1)
+	{
+	    fprintf(stderr, "Error: Unable to calculate standard deviation due to variance error\n");
+	    return -1;
+	}
+
 
 	return (sqrt(variance));
 }
